4edc92da11f87430af6de66641463f968b9d4fbc
Richard <richardbonneau91@gmail.com>
2019-11-15 10:41:16 -0500

    basic signup/login form connected to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 import { BrowserRouter, Route } from "react-router-dom";
 import styled from "styled-components";
-
 import MobileNavBar from "./Components/MobileNavBar";
 import SideBar from "./Components/SideBar";
 import Dashboard from "./Pages/Dashboard";
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import styled from 'styled-components';
 import { myFirebase } from '../utils/fire.js'
@@ -8,15 +8,38 @@ const Container = styled.div`
 `
 
 function Login() {
-    let signup=()=>{
-        myFirebase.auth().createUserWithEmailAndPassword("user@gmail.com","passss").then(user=>{
+    const [signupEmailInput, setSignupEmailInput] = useState("");
+    const [signupPasswordInput, setSignupPasswordInput] = useState("");
+    const [loginEmailInput, setLoginEmailInput] = useState("");
+    const [loginPasswordInput, setLoginPasswordInput] = useState("");
+
+    const firebaseSignup=(e)=>{
+        e.preventDefault();
+        myFirebase.auth().createUserWithEmailAndPassword(signupEmailInput,signupPasswordInput).then(user=>{
             console.log("then user",user)
         })
     }
+    const firebaseLogin=(e)=>{
+        e.preventDefault();
+        myFirebase.auth().signInWithEmailAndPassword(loginEmailInput,loginPasswordInput).then(user=>{
+            console.log("then user",user)
+        })
+    }
+    
   return (
       <Container>
           <h2>signup</h2>
-          <button onClick={signup}>signup</button>
+          <form onSubmit={firebaseSignup}>
+              <input type="text" onChange={(e)=>setSignupEmailInput(e.target.value)} />
+              <input type="text" onChange={(e)=>setSignupPasswordInput(e.target.value)} />
+              <input type="submit" />
+          </form>
+          <h2>login</h2>
+          <form onSubmit={firebaseLogin}>
+              <input type="text" onChange={(e)=>setLoginEmailInput(e.target.value)} />
+              <input type="text" onChange={(e)=>setLoginPasswordInput(e.target.value)} />
+              <input type="submit" />
+          </form>
       </Container>
       
   );
