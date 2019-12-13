3f9afb8155e597d0735919b07257ad34597c163e
richardbonneau <richardbonneau@hotmail.com>
2019-12-12 23:25:09 -0500

    styling sidebar

--- src/Components/MobileNavBar.js
+++ src/Components/MobileNavBar.js
@@ -1,38 +1,45 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-import {FaBars} from 'react-icons/fa'
-import styled from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleHamburgerMenu } from '../_actions';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleHamburgerMenu } from "../_actions";
 
 const Container = styled.div`
-@media (min-width:1024px) {
-    display:none;
-}
-padding: 2px 10px;
-    display:flex;
-    justify-content: space-between;
-    border-bottom:1px solid black;
-`
+  @media (min-width: 1024px) {
+    display: none;
+  }
+  background:purple;
+  padding: 2px 10px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid black;
+  svg{
+      color:white;
+  }
+`;
 const MainLogo = styled.img`
-    height: 25px;
-`
-const styles={
-    hamburgerMenu:{
-        height:'30px',
-        width:'30px',
-        cursor:'pointer'
-    }
-}
+  height: 25px;
+`;
+const styles = {
+  hamburgerMenu: {
+    height: "30px",
+    width: "30px",
+    cursor: "pointer"
+  }
+};
 
 function MobileNavBar() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   return (
-      <Container>
-          <FaBars style={styles.hamburgerMenu} onClick={()=>dispatch(toggleHamburgerMenu())} />
-          <MainLogo src="/images/reduxlogo.png" />
-          <div />
-      </Container>
+    <Container>
+      <FaBars
+        style={styles.hamburgerMenu}
+        onClick={() => dispatch(toggleHamburgerMenu())}
+      />
+      <MainLogo src="/images/reduxlogo.png" />
+      <div />
+    </Container>
   );
 }
 
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,82 +1,116 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaIndent, FaUserAlt, FaMoneyCheckAlt } from 'react-icons/fa';
-import styled from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux'
-import { firebaseLogout, toggleHamburgerMenu } from '../_actions';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaIndent, FaUserAlt, FaMoneyCheckAlt } from "react-icons/fa";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { firebaseLogout, toggleHamburgerMenu } from "../_actions";
 
 const NavContainer = styled.nav`
-@media (min-width:1024px) {
-  left:0px !important;
-  background:purple;
-  color:white;
-}
-z-index: 100;
+  @media (min-width: 1024px) {
+    left: 0px !important;
+    
+  }
+  background: purple;
+  z-index: 100;
   transition: all 300ms ease-out 10ms;
-  background: white;
-  min-width:220px;
-  padding:30px 0 30px 0;
-  display:block;
-  border-right:1px solid black;
-  height:100vh;
-  position:fixed;
+  min-width: 220px;
+  padding: 30px 0 30px 0;
+  display: block;
+  border-right: 1px solid black;
+  height: 100vh;
+  position: fixed;
 `;
 const LinkContainer = styled.div`
-  color:green;
-  display:block;
-  font-size:25px;
+  color: green;
+  display: block;
+  font-size: 25px;
   margin-bottom: 12px;
-  background: ${({ isCurrentPage }) => (isCurrentPage ? "black" : "none")};
+  svg {
+    margin-left: 20px;
+    vertical-align: -4px;
+  }
+  a {
+    color: ${({ isCurrentPage }) => (isCurrentPage ? "white" : "#a9a9a9")};
+  }
+  svg {
+    color: ${({ isCurrentPage }) => (isCurrentPage ? "white" : "#a9a9a9")};
+  }
+  div {
+    display: inline-block;
+    margin-left: 15px;
+  }
+  &:hover {
+    a {
+      color: white;
+    }
+    svg {
+      color: white;
+    }
+  }
 `;
 const MainLogo = styled.img`
-@media (min-width:1024px) {
-  height:100px;
-  margin-bottom:30px;
-  margin-left:45px;
-  display:block;
-}
-  display:none;
-`;
-
-const styles = {
-  icons: {
-    color: 'purple',
-    marginLeft: '20px',
-    verticalAlign: '-4px'
-  },
-  text: {
-    display: 'inline-block',
-    marginLeft: '15px'
+  @media (min-width: 1024px) {
+    height: 100px;
+    margin-bottom: 30px;
+    margin-left: 45px;
+    display: block;
   }
-};
+  display: none;
+`;
 
 function SideBar() {
   const dispatch = useDispatch();
-  const hamburgerMenuOpened = useSelector(state => state.navigation.hamburgerMenuOpened);
-  const currentPage = useSelector(state=>state.navigation.currentPage);
-  const logoutUser = (e) => {
+  const hamburgerMenuOpened = useSelector(
+    state => state.navigation.hamburgerMenuOpened
+  );
+  const currentPage = useSelector(state => state.navigation.currentPage);
+  const logoutUser = e => {
     e.preventDefault();
     dispatch(firebaseLogout());
   };
-  const closeHamburgerMenu = () =>{
+  const closeHamburgerMenu = () => {
     dispatch(toggleHamburgerMenu());
   };
 
   return (
-    <NavContainer style={hamburgerMenuOpened ? { left: '0px' } : { left: '-225px' }}>
+    <NavContainer
+      style={hamburgerMenuOpened ? { left: "0px" } : { left: "-225px" }}
+    >
       <MainLogo src="/images/reduxlogo.png" />
-      <LinkContainer isCurrentPage={currentPage==="/dashboard"} onClick={closeHamburgerMenu}><Link to="/dashboard"><FaIndent style={styles.icons} /><div style={styles.text}>DashBoard</div></Link></LinkContainer>
-      <LinkContainer isCurrentPage={currentPage==="/clients"} onClick={closeHamburgerMenu}><Link to="/clients"><FaUserAlt style={styles.icons} /><div style={styles.text}>Clients</div></Link></LinkContainer>
-      <LinkContainer isCurrentPage={currentPage==="/invoices"} onClick={closeHamburgerMenu}><Link to="/invoices"><FaMoneyCheckAlt style={styles.icons} /><div style={styles.text}>Invoices</div></Link></LinkContainer>
+      <LinkContainer
+        isCurrentPage={currentPage === "/dashboard"}
+        onClick={closeHamburgerMenu}
+      >
+        <Link to="/dashboard">
+          <FaIndent />
+          <div >DashBoard</div>
+        </Link>
+      </LinkContainer>
+      <LinkContainer
+        isCurrentPage={currentPage === "/clients"}
+        onClick={closeHamburgerMenu}
+      >
+        <Link to="/clients">
+          <FaUserAlt />
+          <div>Clients</div>
+        </Link>
+      </LinkContainer>
+      <LinkContainer
+        isCurrentPage={currentPage === "/invoices"}
+        onClick={closeHamburgerMenu}
+      >
+        <Link to="/invoices">
+          <FaMoneyCheckAlt/>
+          <div>Invoices</div>
+        </Link>
+      </LinkContainer>
       {/* <LinkContainer><Link to="/">Calendar</Link></LinkContainer> */}
       {/* <LinkContainer><Link to="/">Contracts</Link></LinkContainer> */}
       {/* <LinkContainer><Link to="/">Tax Report Documents</Link></LinkContainer> */}
       {/* <LinkContainer><Link to="/">Become a Member</Link></LinkContainer> */}
       <button onClick={logoutUser}>Sign out</button>
-
-
     </NavContainer>
   );
-};
+}
 
 export default SideBar;
