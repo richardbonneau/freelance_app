87a38f644de350a9a1bddfdcb7d2349dd29ea7b9
richardbonneau <richardbonneau@hotmail.com>
2020-01-07 14:21:46 -0500

    addtaskpopup complete

--- src/Components/AddTaskPopup.js
+++ src/Components/AddTaskPopup.js
@@ -17,28 +17,29 @@ function AddTaskPopup(props) {
   const dispatch = useDispatch();
   const listOfProjects = useSelector(state => state.projects.projectsList);
   const [nameInput, setNameInput] = useState("");
-  const [selectedProjectId, setSelectedProjectId] = useState(
-    Number(listOfProjects[0].id)
-  );
-  const [projectStartDate, setProjectStartDate] = useState(new Date());
-  const [projectEndDate, setProjectEndDate] = useState(new Date());
+  const [selectedProjectId, setSelectedProjectId] = useState(Number(listOfProjects[0].id));
+  const [workType, setWorkType] = useState("");
+  const [selectedHours, setSelectedHours] = useState("");
+  const [selectedMinutes, setSelectedMinutes] = useState("");
+  const hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  const minutes = ["00", "15", "30", "45"];
 
   const addProjectModalContents = () => {
     const newProjectSubmit = e => {
       // the "frontend" must build the Object that is sent to redux/firebase
       e.preventDefault();
       let newProjectId = Date.now() * 10000 + Math.round(Math.random() * 9999);
-      dispatch(
-        addProjectToFirestore({
-          name: nameInput,
-          id: newProjectId,
-          clientId: selectedClientId,
-          projectDebutDate: firebase.firestore.Timestamp.fromDate(
-            projectStartDate
-          ),
-          projectEndDate: firebase.firestore.Timestamp.fromDate(projectEndDate)
-        })
-      );
+      // dispatch(
+      //   addProjectToFirestore({
+      //     name: nameInput,
+      //     id: newProjectId,
+      //     clientId: selectedClientId,
+      //     projectDebutDate: firebase.firestore.Timestamp.fromDate(
+      //       projectStartDate
+      //     ),
+      //     projectEndDate: firebase.firestore.Timestamp.fromDate(projectEndDate)
+      //   })
+      // );
       props.toggleModal(false);
       setNameInput("");
     };
@@ -50,8 +51,8 @@ function AddTaskPopup(props) {
         <form>
           <label>Project: </label>
           <select
-            value={selectedClientId}
-            onChange={e => setSelectedClientId(Number(e.target.value))}
+            value={selectedProjectId}
+            onChange={e => setSelectedProjectId(Number(e.target.value))}
           >
             {listOfProjects.map((client, i) => (
               <option key={i} value={client.id}>
@@ -59,14 +60,34 @@ function AddTaskPopup(props) {
               </option>
             ))}
           </select>
+          <input
+            type="text"
+            placeholder="Work Type"
+            value={workType}
+            onChange={e => setWorkType(e.target.value)}
+          />
+
+          <select value={selectedHours} onChange={e => setSelectedHours(e.target.value)}>
+            {hours.map((hour, i) => (
+              <option key={i} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          <select value={selectedMinutes} onChange={e => setSelectedMinutes(e.target.value)}>
+            {minutes.map((hour, i) => (
+              <option key={i} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+
           <div />
 
           <div className="modal-buttons">
             {" "}
             <PageButton onClick={newProjectSubmit}>Create Project</PageButton>
-            <PageButton onClick={() => props.toggleModal(false)}>
-              Cancel
-            </PageButton>
+            <PageButton onClick={() => props.toggleModal(false)}>Cancel</PageButton>
           </div>
         </form>
       </ModalContents>
@@ -78,7 +99,7 @@ function AddTaskPopup(props) {
       <MaskOverlay isModalOpened={props.isModalOpened} />
       <ModalContainer
         isModalOpened={props.isModalOpened}
-        style={{ height: "330px", marginTop: "-150px" }}
+        style={{ height: "260px", marginTop: "-130px" }}
       >
         {addProjectModalContents()}
       </ModalContainer>
